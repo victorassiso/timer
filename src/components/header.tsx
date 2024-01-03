8ddeb21596837b5c1edc7277a11346015bcb921a
Victor Assis <victor_assis@poli.ufrj.br>
2024-01-02 22:32:59 -0300

    FEAT: Implement Links in the Header buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import { Scroll, Timer } from 'phosphor-react'
+import { Link } from 'react-router-dom'
 
 import logo from '../assets/logo.svg'
 
@@ -7,19 +8,19 @@ export function Header() {
     <div className="flex h-12 items-end justify-between px-8">
       <img src={logo} alt="" className="h-10 w-10" />
       <div className="flex gap-2">
-        <a
-          href=""
+        <Link
+          to="/"
           className="flex h-12 w-12 items-center justify-center text-primary"
         >
           <Timer className="h-[1.35rem] w-[1.6rem]" />
-        </a>
-        <a
-          href=""
+        </Link>
+        <Link
+          to="/history"
           className="flex h-12 w-12 items-center justify-center text-gray-100"
         >
           {' '}
           <Scroll className="h-[1.35rem] w-[1.6rem]" />
-        </a>
+        </Link>
       </div>
     </div>
   )
