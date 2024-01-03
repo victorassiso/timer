587c9cba0a99306b93473c1cb79644e51e47ff24
Victor Assis <victor_assis@poli.ufrj.br>
2024-01-02 23:06:57 -0300

    FEAT: Implement History List

--- src/pages/history/components/status.tsx
+++ src/pages/history/components/status.tsx
@@ -1,29 +1,15 @@
+import { ReactNode } from 'react'
+
 interface StatusProps {
-  status: 'completed' | 'in progress' | 'interrupted'
+  color: 'green' | 'red' | 'yellow'
+  children: ReactNode
 }
 
-export function Status({ status }: StatusProps) {
-  let color, text
-
-  switch (status) {
-    case 'completed':
-      color = 'green-500'
-      text = 'Completo'
-      break
-    case 'in progress':
-      color = 'yellow-500'
-      text = 'Em andamento'
-      break
-    case 'interrupted':
-      color = 'red-500'
-      text = 'Interrompido'
-      break
-  }
-
+export function Status({ color, children }: StatusProps) {
   return (
     <span className="flex items-center gap-2">
-      <div className={`h-2 w-2 rounded-full bg-${color} content-[]`} />
-      {text}
+      <div className={`bg-${color}-500 h-2 w-2 rounded-full content-[]`} />
+      {children}
     </span>
   )
 }
--- src/pages/history/history.tsx
+++ src/pages/history/history.tsx
@@ -1,11 +1,18 @@
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { useContext } from 'react'
+
+import { CyclesContext } from '../../contexts/cycle-context'
 import { Status } from './components/status'
 
-export function History() {
-  const thStyle =
-    'bg-base-600 text-left p-4 text-base-200 text-sm leading-[1.6] first:rounded-tl-lg first:pl-6 last:rounded-tr-lg last:pr-6'
+const thStyle =
+  'bg-base-600 text-left p-4 text-base-200 text-sm leading-[1.6] first:rounded-tl-lg first:pl-6 last:rounded-tr-lg last:pr-6'
 
-  const tdStyle =
-    'bg-base-700 border-t-base-800 border-t-4 p-4 text-sm leading-[1.6] first:pl-6 last:pr-6 first:w-[50%]'
+const tdStyle =
+  'bg-base-700 border-t-base-800 border-t-4 p-4 text-sm leading-[1.6] first:pl-6 last:pr-6 first:w-[50%]'
+
+export function History() {
+  const { cycles } = useContext(CyclesContext)
 
   return (
     <div className="flex flex-1 flex-col p-20">
@@ -21,14 +28,31 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className={tdStyle}>Tarefa</td>
-              <td className={tdStyle}>20 minutos</td>
-              <td className={tdStyle}>Há 2 meses</td>
-              <td className={tdStyle}>
-                <Status status="in progress" />
-              </td>
-            </tr>
+            {cycles.map((cycle) => (
+              <tr key={cycle.id}>
+                <td className={tdStyle}>{cycle.task}</td>
+                <td className={tdStyle}>{cycle.minutesAmount} minutos</td>
+                <td className={tdStyle}>
+                  {formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </td>
+                <td className={tdStyle}>
+                  {cycle.finishedDate && (
+                    <Status color="green">Concluído</Status>
+                  )}
+
+                  {cycle.interruptedDate && (
+                    <Status color="red">Interrompido</Status>
+                  )}
+
+                  {!cycle.finishedDate && !cycle.interruptedDate && (
+                    <Status color="yellow">Em andamento</Status>
+                  )}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
